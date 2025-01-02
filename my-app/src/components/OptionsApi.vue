94cb4bb63fcc071898b8e876287cbc3097f12254
unknown <fajardillah25@gmail.com>
2025-01-02 22:52:57 +0700

    Options API style

---
+++ my-app/src/components/OptionsApi.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="sayheey()">this is Options API</button>
+  </div>
+</template>
+
+<script>
+
+
+  /** Options API
+    1. menggunakan pendekatan deklaratif berbasis kategori(data, methods, ...)
+    2. menggunakan mixins atau extend
+   */
+  export default {
+    data(){
+      return {
+        message: "hello fajar"
+      }
+    },
+
+    methods: {
+      sayheey(){
+        console.log(this.message)
+      }
+    }
+
+  }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
