13bc9e5bd17646d48a9cecb0715c1f14e24f2846
unknown <fajardillah25@gmail.com>
2025-01-02 22:49:45 +0700

    change file name

---
+++ my-app/src/components/CompositionApi.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- @click adalah event handler -->
+    <button @click="sayHello()">press this</button>
+  </div>
+</template>
+
+<script setup>
+
+  /**
+   * ciri lainya dari compositions
+   * 1. programatik
+   * 2. fungsi dalam setup
+   */
+
+  const hello = "fajar abdillah ahmad";
+
+  //ciri dari composition programming seperti functional programming
+  function sayHello(){
+    console.log(hello)
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
