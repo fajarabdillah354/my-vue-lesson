ff84da07abc5f4b72d302c94d0c2122b19e85dc2
unknown <fajardillah25@gmail.com>
2025-01-03 23:04:46 +0700

    Update

--- my-app/src/components/MainButton.vue
+++ my-app/src/components/MainButton.vue
@@ -1,7 +1,17 @@
-<script setup></script>
+<script setup>
+  const buttonDisable = true
+
+  //atau jika ada banyak value yuang harus diubah kita bisa menggunakan object
+
+  const buttonAttr = {
+    disabled: true,
+    class: 'text4l flex-col-3'
+  }
+
+</script>
 
 <template>
-    <button>Button</button>
+    <button v-bind="buttonAttr">Button</button>
 </template>
 
 <style scoped></style>
